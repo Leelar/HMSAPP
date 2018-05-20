7a08469181cea260d4f4dcf6d4e5d14ebdf2862c
leelar <leelarthor@gmail.com>
2018-05-20 17:32:14 +0700

    ua tshiab lawm nawb

---
+++ src/com/mlk/controllers/DatabaseManager.java
@@ -0,0 +1,141 @@
+package com.mlk.controllers;
+
+import com.mlk.models.Patient;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DatabaseManager {
+
+    String db_connect_string = "jdbc:sqlserver://KOR-PC:1433;databaseName=hmsdb;selectMethod=cursor";
+    String db_userid = "sa";
+    String db_password = "kor";
+    Connection conn = null;
+
+    public DatabaseManager() {
+    }
+
+//    public String getPassword(String domain, String username) throws SQLException, ClassNotFoundException {
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        conn = DriverManager.getConnection(db_connect_string, db_userid, db_password);
+//        String pw = null;
+//        Statement statement = conn.createStatement();
+//        String queryString = "select * from " + domain + " where username = '" + username + "';";
+//        ResultSet rs = statement.executeQuery(queryString);
+//        while (rs.next()) {
+//            pw = rs.getString(3).replaceAll(" ", "");
+//        }
+//        return pw;
+//    }
+    public void insert(Patient pa) throws ClassNotFoundException, SQLException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        conn = DriverManager.getConnection(db_connect_string, db_userid, db_password);
+        if (conn != null) {
+            System.out.println("Succesfull!");
+            String queryString = "INSERT INTO Patient (code, name, surname, gender) "
+                    + "VALUES(?,?, ?,?)";
+            PreparedStatement preparedStmt = conn.prepareStatement(queryString);
+            preparedStmt.setString(1, pa.getCode());
+            preparedStmt.setString(2, pa.getName());
+            preparedStmt.setString(3, pa.getSurname());
+            preparedStmt.setString(4, pa.getGender());
+            preparedStmt.execute();
+            conn.close();
+        } else {
+            System.out.println("Failed");
+        }
+    }
+
+//   public Exam createExamObj(String eid) throws SQLException, ClassNotFoundException, ParseException
+//   {
+//	   String cid=null; 
+//	   Date starttime=null, endtime=null;   
+//	   int maxnum=0, number=0;    
+//	   String period=null;
+//	   String paper=null;
+//	   DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//	   
+//	   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//	   conn = DriverManager.getConnection(db_connect_string,db_userid, db_password);
+//	   
+//       Statement statement = conn.createStatement();
+//       String queryString = "select * from Exam where eid = '"+eid+"';";
+//       ResultSet rs = statement.executeQuery(queryString);
+//       while (rs.next()) {
+//          cid=rs.getString(2).replaceAll(" ", "");
+//          starttime=sdf.parse(rs.getString(3));
+//          maxnum=rs.getInt(4);
+//          number=rs.getInt(5);
+//          paper = rs.getString(6).replaceAll(" ", "");
+//          period = rs.getString(7).replaceAll(" ", "");
+//          endtime=sdf.parse(rs.getString(8));
+//       }
+//       Exam exm = new Exam(eid,cid,starttime,maxnum,number,paper,period,endtime);
+//       return exm;
+//   }
+//   public Student createStudentObj(String username) throws SQLException, ClassNotFoundException, ParseException
+//   {
+//	   String matricno=null, password=null, realname=null;
+//	   byte [] profilepic=null;
+//	   
+//	   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//	   conn = DriverManager.getConnection(db_connect_string,db_userid, db_password);   
+//           Statement statement = conn.createStatement();
+//           String queryString = "select * from Student where username = '"+username+"';";
+//           ResultSet rs = statement.executeQuery(queryString);
+//            while (rs.next()) {
+//            matricno=rs.getString(1).replaceAll(" ", "");
+//            password=rs.getString(3).replaceAll(" ", "");
+//            realname=rs.getString(4).replaceAll(" ", "");
+//            profilepic=rs.getBytes(5);
+//          }
+//          Student std=new Student(matricno,username,password,realname,profilepic);
+//          return std;
+//   }
+//   
+//   public Course createCourseObj(String cid) throws SQLException, ClassNotFoundException, ParseException
+//   {
+//	   String cname=null, description=null;
+//	   
+//	   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//	   conn = DriverManager.getConnection(db_connect_string,db_userid, db_password);
+//	   
+//       Statement statement = conn.createStatement();
+//       String queryString = "select * from Course where cid = '"+cid+"';";
+//       ResultSet rs = statement.executeQuery(queryString);
+//       while (rs.next()) {
+//          cname=rs.getString(2).replaceAll(" ", "");
+//          description=rs.getString(3);
+//       }
+//       Course cs = new Course(cid, cname, description);
+//       return cs;
+//   }
+//   
+//   public Proctor createProctorObj(String username) throws SQLException, ClassNotFoundException, ParseException
+//   {
+//	   String pid=null, password=null, realname=null;
+//	   byte [] profilepic=null;
+//	   
+//	   Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//	   conn = DriverManager.getConnection(db_connect_string,db_userid, db_password);
+//	   
+//       Statement statement = conn.createStatement();
+//       String queryString = "select * from Proctor where username = '"+username+"' ;";
+//       ResultSet rs = statement.executeQuery(queryString);
+//       while (rs.next()) {
+//          pid=rs.getString(1).replaceAll(" ", "");
+//          password=rs.getString(3).replaceAll(" ", "");
+//          realname=rs.getString(4).replaceAll(" ", "");
+//          profilepic=rs.getBytes(5); 
+//       }
+//       Proctor pro=new Proctor(pid,username,password,realname,profilepic);
+//       return pro;
+//   }
+}
---
+++ src/com/mlk/views/FrmPosition.java
@@ -0,0 +1,166 @@
+
+package com.mlk.views;
+
+import java.awt.Font;
+
+public class FrmPosition extends javax.swing.JInternalFrame {
+    public FrmPosition() {
+        initComponents();
+        jTable1.getTableHeader().setFont(new Font("Saysettha OT",Font.BOLD,12));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setClosable(true);
+        setTitle("Position");
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel2.setLayout(new javax.swing.BoxLayout(jPanel2, javax.swing.BoxLayout.LINE_AXIS));
+
+        jButton1.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jButton1.setText("ສ້າງໃໝ່");
+        jPanel2.add(jButton1);
+
+        jButton2.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jButton2.setText("ບັນທືກ");
+        jPanel2.add(jButton2);
+
+        jButton3.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jButton3.setText("ຍົກເລີກ");
+        jPanel2.add(jButton3);
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel1.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jLabel1.setText("ລະຫັດ");
+
+        jTextField1.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+
+        jTextField2.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+
+        jLabel2.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jLabel2.setText("ຊື່ຕຳແໜ່ງ");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(5, 5, 5)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(2, 2, 2)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(5, 5, 5))
+        );
+
+        jPanel4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel4.setLayout(new java.awt.BorderLayout());
+
+        jTable1.setFont(new java.awt.Font("Saysettha OT", 0, 12)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null}
+            },
+            new String [] {
+                "ລະຫັດ", "ຊື່ຕຳແໜ່ງ"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
+        jTable1.setRowHeight(27);
+        jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setMinWidth(70);
+            jTable1.getColumnModel().getColumn(0).setMaxWidth(70);
+            jTable1.getColumnModel().getColumn(1).setMinWidth(400);
+            jTable1.getColumnModel().getColumn(1).setMaxWidth(400);
+        }
+
+        jPanel4.add(jScrollPane1, java.awt.BorderLayout.CENTER);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, 478, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(2, 2, 2)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 424, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    // End of variables declaration//GEN-END:variables
+}
